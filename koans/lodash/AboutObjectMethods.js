a07537841bfc1112234c4f5b957364099408b7c6
Bret Davidson <bret_davidson@ncsu.edu>
2016-01-07 16:22:14 -0500

    add object methods

--- koans/lodash/AboutObjectMethods.js
+++ koans/lodash/AboutObjectMethods.js
@@ -1,4 +1,28 @@
-// _.mapKeys
-// _.mapValues
 // _.merge
 // _.pick
+// _.get
+// _.set
+
+describe("About Object Methods", function() {
+
+  //We shall contemplate truth by testing reality, via spec expectations.
+  it("should recursively merge own enumerable properties of the source object(s)", function () {
+    var users = {
+      'data': [{ 'user': 'barney' }, { 'user': 'fred' }]
+    };
+
+    var ages = {
+      'data': [{ 'age': 36 }, { 'age': 40 }]
+    };
+
+    expect(_.merge(users, ages)).toEqual({'data': [{'user': 'barney', 'age': 36}, {'user': 'fred', 'age': 40}]});
+  });
+
+  it("should creates an object composed of the picked object properties", function () {
+    var object = { 'user': 'fred', 'age': 40 };
+
+    expect(_.pick(object, 'user')).toEqual({ 'user': 'fred' });
+  });
+
+
+});
