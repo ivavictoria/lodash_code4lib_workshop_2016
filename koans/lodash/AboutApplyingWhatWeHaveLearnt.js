ad6684d201b8d6e71fed8fc9dcb0d532485d223c
Bret Davidson <bret_davidson@ncsu.edu>
2016-01-15 15:46:46 -0500

    setup map stuff

--- koans/lodash/AboutApplyingWhatWeHaveLearnt.js
+++ koans/lodash/AboutApplyingWhatWeHaveLearnt.js
@@ -6,6 +6,20 @@ describe("About Applying What We Have Learnt", function () {
       attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
+    _.each(clean_beer, function(beer) {
+      L.marker([beer.lat, beer.long]).addTo(map).bindPopup("beers");
+    });
+
     expect(true).toEqual(true);
+    alert('hello')
+    beer_data = _.cloneDeep(clean_beer)
+    alert(_.isEqual(beer_data, clean_beer))
+    if (_.isEqual(beer_data, clean_beer)) {
+      alert('EQUAL')
+    } else {
+      alert("NOT EQUQAL")
+    }
   });
+
+
 });
