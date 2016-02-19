42a0ec9aa8829536ed0ee7a3b6c6b283516d0e0c
Bret Davidson <bret_davidson@ncsu.edu>
2016-02-19 16:39:19 -0500

    clean up files

--- koans/lodash/AboutApplyingWhatWeHaveLearnt.js
+++ koans/lodash/AboutApplyingWhatWeHaveLearnt.js
@@ -3,15 +3,15 @@ describe("About Applying What We Have Learnt", function () {
     if (_.isEqual(beer_data, clean_beer)) {
       drawMap();
     }
-  })
+  });
 
 
   // Methods to test?
   // _.capitalize (_.map over array)
   // phone number manipulation
   it("should test out some stuff", function () {
-    
-    beer_data = _.cloneDeep(clean_beer)
+
+    beer_data = _.cloneDeep(clean_beer);
     expect(true).toEqual(true);
 
   });
